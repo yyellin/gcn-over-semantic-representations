1329abf7d0e0e8b28aa8745f8b518852da33e2f7
yyellin <yyellin@gmail.com>
2020-04-08 18:54:06 +0300

    trainer.predict redurns a tuple of four instead of three (we ignore the fourth return value through)

--- train.py
+++ train.py
@@ -154,7 +154,7 @@ for epoch in range(1, opt['num_epoch']+1):
     predictions = []
     dev_loss = 0
     for i, batch in enumerate(dev_batch):
-        preds, _, loss = trainer.predict(batch)
+        preds, _, loss, _ = trainer.predict(batch)
         predictions += preds
         dev_loss += loss
     predictions = [id2label[p] for p in predictions]
