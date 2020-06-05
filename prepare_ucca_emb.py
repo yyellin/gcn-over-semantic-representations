b3963a7378c89b45930d0de88fe4f2a922396282
yyellin <yyellin@gmail.com>
2020-06-05 13:22:13 +0300

    vocabulary of tokens and ucca-embeddings should be based on train only - that's because the embeddings change during training, but dev/train embedding would remain untouched.

--- prepare_ucca_emb.py
+++ prepare_ucca_emb.py
@@ -29,7 +29,8 @@ def main():
 
     helper.ensure_dir(args.ucca_embedding_dir)
 
-    UccaEmbedding.prepare(args.ucca_embedding_dim, [train_file, dev_file, test_file], index_file, embedding_file)
+    # similarly to vocab, we'll only load encodings from train
+    UccaEmbedding.prepare(args.ucca_embedding_dim, [train_file], index_file, embedding_file)
 
     return UccaEmbedding(args.ucca_embedding_dim, index_file, embedding_file)
 
