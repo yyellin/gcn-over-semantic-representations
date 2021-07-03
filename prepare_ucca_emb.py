876a1ab54c097fb945f049c74211782a4d964996
yyellin <yyellin@gmail.com>
2021-07-03 12:07:45 +0300

    better arguments for prepare_ucca_emb

--- prepare_ucca_emb.py
+++ prepare_ucca_emb.py
@@ -7,7 +7,7 @@ from utils import helper
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Prepare vocab for relation extraction.')
-    parser.add_argument('--data_dir', default=r'C:\Users\JYellin\re_1\tacred\data\json-enhanced3', help='TACRED directory.')
+    parser.add_argument('data_dir', help='Enhanced TACRED directory.')
     parser.add_argument('--ucca_embedding_dim', type=int, default=80, help='UCCA Path to Root Emdedding vector dimension.')
     parser.add_argument('--ucca_embedding_dir', default=r'./ucca-embedding', help='Output vocab directory.')
     parser.add_argument('--ucca_embedding_file', default='ucca_path_embeddings', help='UCCA Path to Root Embedding vector file')
